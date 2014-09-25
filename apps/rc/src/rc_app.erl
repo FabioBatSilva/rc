ea8ea3601723534f8d341bae9ee08572dcf74466
Alexey Aniskin <alexey.aniskin@gmail.com>
2014-09-25 10:26:08 +0600

    initial

---
+++ apps/rc/src/rc_app.erl
@@ -0,0 +1,23 @@
+-module(rc_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    case rc_sup:start_link() of
+        {ok, Pid} ->
+            ok = riak_core:register([{vnode_module, rc_vnode}]),
+            ok = riak_core_node_watcher:service_up(rc, self()),
+            {ok, Pid};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+stop(_State) ->
+    ok.
